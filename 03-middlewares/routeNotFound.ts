bb0912bca04abeae97f2117a726e860284142e1a
itaiglikman <itaig1998@gmail.com>
2025-08-29 13:49:43 +0300

    chore: add to project setup

--- 03-middlewares/routeNotFound.ts
+++ 03-middlewares/routeNotFound.ts
@@ -2,11 +2,9 @@ import { NextFunction, Request, Response } from "express";
 import { RouteNotFoundError } from "../01-utils/client-errors";
 
 function routeNotFound(request: Request, response: Response, next: NextFunction) {
-
     // Handles 404 - route not found
-    console.error('Error: route not found');
+    console.error(`Error: route ${request.originalUrl} not found, method: ${request.method}`);
     throw new RouteNotFoundError(request.originalUrl)
-    // response.status(404).send('Not found ' + request.originalUrl);
 }
 
 export default routeNotFound;
